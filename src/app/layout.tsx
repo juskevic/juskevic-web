e43b16191c605b08027cc900b8996211121d0300
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-17 12:30:15 +0200

    Add metadata and viewport settings to layout.tsx
    Enhanced the metadata with additional details including creator, publisher, category, and keywords to better optimize the website for search engines. Also, added viewport settings to improve the website's responsiveness and user experience on different devices.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
@@ -9,10 +9,77 @@ import Header from "@/components/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+}
+
 export const metadata: Metadata = {
-  title: "Max Juškevič",
-  description: "Max's portfolio website and blog. 💫",
-};
+    title: "Max Juškevič",
+    description: "Max's portfolio website and blog. 💫",
+    creator: "Max Juškevič",
+    publisher: "Max Juškevič",
+    category: 'technology',
+    icons: {
+      icon: '/favicon.ico',
+    },
+    keywords: [
+        "Frontend Development",
+        "Web Development",
+        "UI/UX Design",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Vue.js",
+        "Angular",
+        "Responsive Design",
+        "User Experience",
+        "User Interface",
+        "Web Applications",
+        "E-commerce Websites",
+        "Portfolio Website",
+        "Blog",
+        "Web Design Trends",
+        "Frontend Frameworks",
+        "Accessibility",
+        "Performance Optimization",
+        "Cross-Browser Compatibility",
+        "Mobile First Approach",
+        "Interactive Web Elements",
+        "Animation",
+        "Single Page Applications",
+        "API Integration",
+        "State Management",
+        "Component-Based Architecture"
+    ],
+    openGraph: {
+        images: '',
+        title: "Max Juškevič: Frontend Engineer",
+        description: "Max's portfolio website and blog. 💫",
+        url: 'https://juskevic.com',
+        siteName: 'Max Juškevič',
+        type: 'website',
+    },
+    twitter: {
+        title: 'Max Juškevič: Frontend Engineer',
+        description: 'Max\'s portfolio website and blog. 💫',
+    },
+    robots: {
+        index: false,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: false,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    }
+}
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
   return (
