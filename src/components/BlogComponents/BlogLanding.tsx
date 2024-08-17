b38da8aede67ac28a4bf34c6287bdbf44a6ebb8e
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-17 17:36:31 +0200

    Refactor BlogLanding layout and styles
    Updated the layout for better spacing and responsive behavior across different screen sizes. Improved typography and visual details such as font sizes and spacing. Enhanced the appearance of the latest blog post card by adjusting dimensions and adding border styling.

--- src/components/BlogComponents/BlogLanding.tsx
+++ src/components/BlogComponents/BlogLanding.tsx
@@ -12,9 +12,9 @@ export default function BlogLanding() {
     return (
         <>
             <div className="flex justify-center">
-                <div className="flex flex-col md:flex-row justify-evenly w-full">
-                    <div className="">
-                        <h1 className={`${poppinsFont.className} font-bold text-2xl md:text-4xl xl:text-6xl space-y-0.5`}>
+                <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 md:justify-evenly w-full">
+                    <div>
+                        <h1 className={`${poppinsFont.className} font-bold text-4xl xl:text-6xl space-y-1`}>
                             {/*{"Thoughts on Code, Design, and Other Stuff"}*/}
                             <div className="animate-fade-down animate-delay-[200ms]">{"Thoughts on"}</div>
                             <div className="opacity-90 animate-fade-down animate-delay-[400ms]">{"Code, Design,"}</div>
@@ -22,20 +22,20 @@ export default function BlogLanding() {
                         </h1>
                     </div>
                     <div>
-                        <div className="text-lg text-gray-500 flex flex-row gap-1 pb-3 animate-fade-down animate-delay-[700ms]">
+                        <div className="text-xl flex flex-row gap-1 pb-2 md:pb-3 animate-fade-down animate-delay-[700ms]">
                             <span>{"Latest blog post"}</span>
                             <span><IconNews stroke={1.5} /></span>
                         </div>
-                        <Card isHoverable isPressable className="animate-fade-down animate-delay-[800ms] shadow-none bg-white">
+                        <Card isHoverable isPressable className="animate-fade-down animate-delay-[800ms] border-1 border-gray-300 shadow-none">
                             <CardHeader className="z-10 top-1 flex-col !items-start pb-0">
                                 <h4 className="text-black font-medium text-2xl">Latest blog post title</h4>
                             </CardHeader>
-                            <CardBody className="">
-                                <Image height={200} width={300} src="placeholder.svg" alt="placeholder image"></Image>
+                            <CardBody className="md:w-[250px] md:h-[150px] xl:w-[350px] xl:h-[250px]">
+                                <Image className="w-full h-full" src="placeholder.svg" alt="placeholder image"></Image>
                             </CardBody>
                             <CardFooter className="flex flex-col !items-start z-10">
                                 <div className="flex flex-row text-blue-500">
-                                    <ScrollShadow hideScrollBar={true} className="space-x-2 max-w-[300px]" orientation="vertical">
+                                    <ScrollShadow hideScrollBar={true} className="space-x-2 max-w-[200px] xl:max-w-[300px]" orientation="vertical">
                                         <span>{"#frontend"}</span>
                                         <span>{"#api"}</span>
                                         <span>{"#blog"}</span>
