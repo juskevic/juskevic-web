a9aa974012d2d2b828289853588ba3f9906bf6c0
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-22 21:38:22 +0200

    Refactor Header component layout and remove menu toggle
    Removed the unused menu toggle functionality and adjusted the layout of the Header component. Updated ButtonGroup to be hidden for small screens and visible for medium and above.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,35 +1,30 @@
 'use client'
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Button, ButtonGroup} from "@nextui-org/react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    Button,
+    ButtonGroup,
+    Chip
+} from "@nextui-org/react";
 import {Link} from "react-scroll";
 import {IconFolders, IconMessages, IconPencil, IconUserCircle} from '@tabler/icons-react';
 
 export default function Header( ) {
 
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
     return (
-        <Navbar onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll={true}>
-
-            <NavbarContent>
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">Max Juškevič</p>
-                </NavbarBrand>
-            </NavbarContent>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <ButtonGroup radius="lg" variant="light">
+        <Navbar shouldHideOnScroll={true}>
+            <NavbarContent></NavbarContent>
+            <NavbarContent className="gap-4" justify="center">
+                <ButtonGroup radius="lg" variant="light" className="hidden md:flex">
                     <Button as={Link} activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="text-medium" endContent={<IconUserCircle stroke={1.5} />}>About</Button>
                     <Button as={Link} activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500} className="text-medium" endContent={<IconFolders stroke={1.5} />}>Projects</Button>
                     <Button as={Link} activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500} className="text-medium" endContent={<IconMessages stroke={1.5} />}>Contact</Button>
                 </ButtonGroup>
                 <Button as="a" href="/blog" variant="flat" className="text-medium" endContent={<IconPencil stroke={1.5} />}>Blog</Button>
             </NavbarContent>
-
-            <NavbarMenu>
-            </NavbarMenu>
-
+            <NavbarContent className="hidden md:flex"></NavbarContent>
         </Navbar>
     );
 }
