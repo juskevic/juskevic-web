1627e37e3a580f89224aa66866d58ae2cdce5983
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-28 13:49:55 +0200

    Add CV download link and adjust animation delays
    Added a new "DOWNLOAD CV" link with an icon in the header. Adjusted the animation delays for the "About", "Projects", and "Contact" tabs to improve timing consistency.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 'use strict'
 import React from "react";
-import {IconPointFilled} from "@tabler/icons-react";
+import {IconFileDownload, IconPointFilled} from "@tabler/icons-react";
+import {Link} from "@nextui-org/react";
 
 export default function Header() {
 
@@ -15,10 +16,18 @@ export default function Header() {
     const currentDateTime = calculatePragueDateTime();
 
     return (
-        <header className="md:text-xl flex justify-between p-5 bg-black animate-fade animate-once animate-duration-[3000ms] animate-ease-in-out">
-            <p className="text-primary">MAX JUŠKEVIČ</p>
+        <header
+            className="md:text-xl flex justify-between p-5 bg-black animate-fade animate-once animate-duration-[3000ms] animate-ease-in-out">
+            <div className="flex flex-row gap-20">
+                <p className="text-primary">MAX JUŠKEVIČ</p>
+                <div className="flex flex-row gap-1">
+                    <Link underline="hover" href="/" className="text-secondary text-xl">DOWNLOAD CV</Link>
+                    <IconFileDownload className="text-secondary" stroke={1.5}/>
+                </div>
+            </div>
             <div className="flex flex-row gap-1">
-                <IconPointFilled className="animate-ping animate-infinite animate-duration-[1500ms] animate-ease-linear text-secondary my-auto"/>
+                <IconPointFilled
+                    className="animate-ping animate-infinite animate-duration-[1500ms] animate-ease-linear text-green-500 my-auto"/>
                 <p className="text-primary">
                     <span>{"PRAGUE, CZECHIA "}</span><span>{currentDateTime}</span>
                 </p>
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -15,13 +15,13 @@ export default function Main() {
                 <Tab key="landing" title="LANDING" className="animate-fade-right animate-once animate-duration-[2000ms] animate-ease-in-out">
                     <Landing/>
                 </Tab>
-                <Tab key="about" title="ABOUT" className="animate-fade-right animate-once animate-duration-[2000ms] animate-delay-200 animate-ease-in-out">
+                <Tab key="about" title="ABOUT" className="animate-fade-right animate-once animate-duration-[2000ms] animate-delay-300 animate-ease-in-out">
                     <About/>
                 </Tab>
-                <Tab key="projects" title="PROJECTS" className="animate-fade-right animate-once animate-duration-[2000ms] animate-delay-300 animate-ease-in-out">
+                <Tab key="projects" title="PROJECTS" className="animate-fade-right animate-once animate-duration-[2000ms] animate-delay-500 animate-ease-in-out">
                     <Projects />
                 </Tab>
-                <Tab key="contact" title="CONTACT" className="animate-fade-right animate-once animate-duration-[2000ms] animate-delay-500 animate-ease-in-out">
+                <Tab key="contact" title="CONTACT" className="animate-fade-right animate-once animate-duration-[2000ms] animate-delay-700 animate-ease-in-out">
                     <Contact/>
                 </Tab>
             </Tabs>
