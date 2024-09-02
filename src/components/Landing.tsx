0868b5923ee4f60165911417fbc4185fdd2f47b2
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-09-02 17:51:23 +0200

    Remove animations from Landing component
    Animated elements and associated logic were removed for a simplified version of the Landing component. This change reduces complexity and potentially enhances performance by eliminating unnecessary animations.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,8 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {Archivo_Black} from "next/font/google";
 import SocialsButtonGroup from "@/components/ui/SocialsButtonGroup";
 import {Button} from "@nextui-org/react";
-import { motion } from 'framer-motion';
 
 const archivoBlack = Archivo_Black({
     subsets: ['latin'],
@@ -12,93 +11,36 @@ const archivoBlack = Archivo_Black({
 
 export default function Landing() {
 
-    const [hasAnimated, setHasAnimated] = useState(false);
-
-    useEffect(() => {
-        // Ensure the animation only runs once per page load
-        if (!hasAnimated) {
-            setHasAnimated(true);
-        }
-    }, [hasAnimated]);
-
-    const containerVariants = {
-        hidden: { opacity: 0, y: 50 },
-        visible: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                duration: 0.8,
-                ease: 'easeOut',
-                when: 'beforeChildren',
-                staggerChildren: 0.2,
-            },
-        },
-    };
-
-    const textVariants = {
-        hidden: { opacity: 0, y: 20 },
-        visible: { opacity: 1, y: 0 },
-    };
-
-    const buttonVariants = {
-        hidden: { opacity: 0, scale: 0.8 },
-        visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-    };
-
     return (
-        <div className="flex justify-start flex-col gap-8 md:gap-10 xl:gap-14">
-            <motion.div
-                className={`${archivoBlack.className} flex flex-col text-foreground text-6xl md:text-8xl xl:text-9xl`}
-                variants={containerVariants}
-                initial="hidden"
-                animate={hasAnimated ? 'visible' : 'hidden'}
-            >
-                <motion.div variants={textVariants}>
-                    <p className="hollow-text hover:text-primary-100 transition">{"MAX"}</p>
-                </motion.div>
-                <motion.div variants={textVariants}>
-                    <p className="hollow-text hover:text-primary-100 transition">{"JUŠKEVIČ"}</p>
-                </motion.div>
-            </motion.div>
-            <motion.div
-                className="flex text-3xl md:text-5xl xl:text-6xl text-foreground"
-                variants={textVariants}
-                initial="hidden"
-                animate={hasAnimated ? 'visible' : 'hidden'}
-            >
-                <p>{"Frontend Engineer"}</p>
-            </motion.div>
-            <motion.div
-                className="flex text-lg md:text-xl xl:text-2xl md:w-2/3 xl:w-1/2"
-                variants={textVariants}
-                initial="hidden"
-                animate={hasAnimated ? 'visible' : 'hidden'}
-            >
-                <p>
-                    {"I'm a passionate Web Developer specializing in creating dynamic and user-friendly websites and applications. With a focus on modern frontend technologies, I deliver high-quality code and engaging user experiences."}
-                </p>
-            </motion.div>
-            <motion.div
-                className="flex flex-row gap-5"
-                variants={containerVariants}
-                initial="hidden"
-                animate={hasAnimated ? 'visible' : 'hidden'}
-            >
-                <motion.div variants={buttonVariants}>
-                    <SocialsButtonGroup/>
-                </motion.div>
-                <motion.div variants={buttonVariants}>
-                    <Button
-                        size="md"
-                        variant="flat"
-                        color="primary"
-                        className="text-lg"
-                    >
-                        {"Get in Touch"}
-                    </Button>
-                </motion.div>
-            </motion.div>
-        </div>
+        <>
+            <div className="flex justify-start flex-col gap-8 md:gap-10 xl:gap-14">
+                <div className={`${archivoBlack.className} flex flex-col text-foreground text-6xl md:text-8xl xl:text-9xl`}>
+                    <div><p className="hollow-text hover:text-primary-100 transition">{"MAX"}</p></div>
+                    <div><p className="hollow-text hover:text-primary-100 transition">{"JUŠKEVIČ"}</p></div>
+                </div>
+                <div className="flex text-3xl md:text-5xl xl:text-6xl text-foreground">
+                    <p>{"Frontend Engineer"}</p>
+                </div>
+                <div className="flex text-lg md:text-xl xl:text-2xl md:w-2/3 xl:w-1/2">
+                    <p>
+                        {"I'm a passionate Web Developer specializing in creating dynamic and user-friendly websites and applications. With a focus on modern frontend technologies, I deliver high-quality code and engaging user experiences."}
+                    </p>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <div><SocialsButtonGroup/></div>
+                    <div>
+                        <Button
+                            size="md"
+                            variant="flat"
+                            color="primary"
+                            className="text-lg"
+                        >
+                            {"Get in Touch"}
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
