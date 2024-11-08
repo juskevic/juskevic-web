065207f95b2f605f8cb10354aa24dfc00b64f2c8
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-11-08 11:58:21 +0100

    Add dedicated layout and header for blog section
    Introduced a tailor-made layout and header for the blog section to provide a consistent and optimized user experience. Adjusted the main layout and providers to conditionally display different headers and footers based on the current path.

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,78 @@
+import type { Metadata, Viewport } from "next";
+import "../globals.css";
+import React from "react";
+import { Montserrat } from 'next/font/google';
+
+import Footer from "@/components/layout/Footer";
+import BlogHeader from "@/app/blog/components/blogHeader";
+
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['400'],
+});
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+}
+
+export const metadata: Metadata = {
+    title: "Max Juškevič Blog | Frontend Insights & Web Development",
+    description: "Explore blog posts by Max Juškevič on frontend development, modern web practices, and insights into web technologies like React and Vue.",
+    creator: "Max Juškevič",
+    publisher: "Max Juškevič",
+    category: 'technology',
+    icons: {
+        icon: '/favicon.ico',
+    },
+    keywords: [
+        "Frontend Development Blog",
+        "Web Development Insights",
+        "React Blog",
+        "Vue.js",
+        "JavaScript Tips",
+        "UI/UX Design",
+        "HTML",
+        "CSS",
+        "Responsive Design",
+        "Performance Optimization",
+        "SEO for Web Developers",
+        "Modern Web Applications",
+        "Max Juškevič Blog"
+    ],
+    openGraph: {
+        title: "Max Juškevič Blog | Frontend Insights & Web Development",
+        description: "Stay updated with the latest insights and tutorials on frontend development and modern web practices from Max Juškevič.",
+        url: 'https://juskevic.com/blog',
+        siteName: 'Max Juškevič Blog',
+        type: 'website',
+    },
+    twitter: {
+        title: 'Max Juškevič Blog | Frontend Insights & Web Development',
+        description: 'Follow the latest blog posts by Max Juškevič on frontend development, modern web apps, and performance optimization.',
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    }
+}
+
+export default function BlogLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className={`${montserrat.className}`}>
+            <BlogHeader />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    );
+}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,22 @@
 'use client'
 
+import Header from '@/components/layout/Header';
 import {NextUIProvider} from '@nextui-org/react';
-import {useRouter} from 'next/navigation'
+import {usePathname, useRouter} from 'next/navigation'
 import React from "react";
+import Footer from "@/components/layout/Footer";
 
 export function Providers({children}: { children: React.ReactNode }) {
     const router = useRouter();
+    const pathname = usePathname();
+
+    const showHeader = !pathname.startsWith('/blog')
 
     return (
         <NextUIProvider navigate={router.push}>
+            {showHeader && <Header/>}
             {children}
+            {showHeader && <Footer/>}
         </NextUIProvider>
     )
 }
